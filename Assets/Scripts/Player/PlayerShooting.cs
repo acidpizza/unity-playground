b5f5c33c5967c876eaaf5892a1ce1d2ad0a91a30
jstym <jstym@noemail.com>
2015-06-20 23:47:39 +0800

    Added hits from shooting.
    - Obstacles with shootable layer to be hit
    - Particle smoke on hit
    - Prevent shooting through obstacles if too close.

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -5,24 +5,31 @@ public class PlayerShooting : MonoBehaviour
     public int damagePerShot = 20;
     public float timeBetweenBullets = 0.15f;
     public float range = 100f;
-
-
+	
     float timer;
     Ray shootRay;
-    RaycastHit shootHit;
+	Ray shootRay_check;
+	Vector3 playerMiddlePosition;
+	RaycastHit shootHit;
     int shootableMask;
-    ParticleSystem gunParticles;
-    LineRenderer gunLine;
-    AudioSource gunAudio;
+	bool hit = false;
+	float gunTipOverExtension = 0.7f;
+
+	public ParticleSystem gunParticles;
+	public ParticleSystem hitParticles;
+
+	LineRenderer gunLine;
+	AudioSource gunAudio;
     Light gunLight;
     float effectsDisplayTime = 0.2f;
-
 
     void Awake ()
     {
         shootableMask = LayerMask.GetMask ("Shootable");
-        gunParticles = GetComponent<ParticleSystem> ();
-        gunLine = GetComponent <LineRenderer> ();
+        //gunParticles = GetComponent<ParticleSystem> ();
+		//hitParticles = GetComponentInChildren <ParticleSystem> ();
+
+		gunLine = GetComponent <LineRenderer> ();
         gunAudio = GetComponent<AudioSource> ();
         gunLight = GetComponent<Light> ();
     }
@@ -54,34 +61,55 @@ public class PlayerShooting : MonoBehaviour
     void Shoot ()
     {
         timer = 0f;
-
         gunAudio.Play ();
-
         gunLight.enabled = true;
-
         gunParticles.Stop ();
         gunParticles.Play ();
 
-        gunLine.enabled = true;
-        gunLine.SetPosition (0, transform.position);
+		hit = false;
+		//check if player has cut through an obstacle
+		playerMiddlePosition = transform.position;
+		playerMiddlePosition.z -= gunTipOverExtension; // distance from player middle to gun barrel is 0.7f
+		shootRay_check.origin = playerMiddlePosition;
+		shootRay_check.direction = transform.forward;
+		if(Physics.Raycast (shootRay_check, out shootHit, gunTipOverExtension, shootableMask))
+		{
+			// Player has cut through an obstacle. Don't shoot past it.
+			gunLine.enabled = false;
+			hit = true;
+		}
+        else 
+		{
+			gunLine.enabled = true;
+			gunLine.SetPosition (0, transform.position);
 
-        shootRay.origin = transform.position;
-        shootRay.direction = transform.forward;
+			// check if anything was hit
+			shootRay.origin = transform.position + (transform.forward * -1);
+			shootRay.direction = transform.forward; // check forward
+			if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+	        {
+				// Something was hit -> truncate laser length and play hit particles
+	            gunLine.SetPosition (1, shootHit.point);
+				hit = true;
+	        }
+	        else
+	        {
+				// Nothing was hit -> shoot to maximum range
+				gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+	        }
+		}
 
-        if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
-        {
-/*
+		if(hit)
+		{
+			hitParticles.transform.position = shootHit.point;
+			hitParticles.Play();
+			/*
 			EnemyHealth enemyHealth = shootHit.collider.GetComponent <EnemyHealth> ();
-            if(enemyHealth != null)
-            {
-                enemyHealth.TakeDamage (damagePerShot, shootHit.point);
-            }
-*/
-            gunLine.SetPosition (1, shootHit.point);
-        }
-        else
-        {
-            gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
-        }
+	        if(enemyHealth != null)
+	        {
+	            enemyHealth.TakeDamage (damagePerShot, shootHit.point);
+	        }
+			*/
+		}
     }
 }
