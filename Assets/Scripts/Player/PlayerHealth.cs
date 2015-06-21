70c4f393dc0a0f9cb5dca254922bdef4778c4aca
jstym <jstym@noemail.com>
2015-06-22 00:30:38 +0800

    Added UI for health and score monitoring, and gameover screen with auto restart.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -6,8 +6,8 @@ public class PlayerHealth : MonoBehaviour
 {
     public int startingHealth = 100;
     public int currentHealth;
-//    public Slider healthSlider;
-//    public Image damageImage;
+    public Slider healthSlider;
+    public Image damageImage;
     public AudioClip deathClip;
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
@@ -35,11 +35,11 @@ public class PlayerHealth : MonoBehaviour
     {
         if(damaged)
         {
-//            damageImage.color = flashColour;
+            damageImage.color = flashColour;
         }
         else
         {
-//            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+            damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
         }
         damaged = false;
     }
@@ -51,7 +51,7 @@ public class PlayerHealth : MonoBehaviour
 
         currentHealth -= amount;
 
-//        healthSlider.value = currentHealth;
+        healthSlider.value = currentHealth;
 
         playerAudio.Play ();
 
