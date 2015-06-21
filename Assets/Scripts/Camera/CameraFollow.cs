3444f66da70b773b9076fea44d1e8f62b4dca7a3
jstym <jstym@noemail.com>
2015-06-21 18:21:57 +0800

    Added camera in-game zoom.

--- Assets/Scripts/Camera/CameraFollow.cs
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -7,13 +7,29 @@ public class CameraFollow : MonoBehaviour {
 
 	Vector3 offset;
 
+	public float zoomSensitivity = 15.0f;
+	public float zoomSpeed = 5.0f;
+	public float zoomMin = 5.0f;
+	public float zoomMax = 40.0f;
+	private float zoom;
+	Camera camera;
+
 	void Start()
 	{
+		camera = GetComponent<Camera>();
+		zoom = camera.fieldOfView;
 		offset = transform.position - target.position;
 	}
 
-	void FixedUpdate()
+	void Update() 
 	{
+		zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
+		zoom = Mathf.Clamp(zoom, zoomMin, zoomMax);
+	}
+
+	void LateUpdate()
+	{
+		camera.fieldOfView = Mathf.Lerp (camera.fieldOfView, zoom, Time.deltaTime * zoomSpeed);
 		Vector3 targetCamPos = target.position + offset;
 		transform.position = Vector3.Lerp (transform.position, targetCamPos, smoothing * Time.deltaTime);
 	}
