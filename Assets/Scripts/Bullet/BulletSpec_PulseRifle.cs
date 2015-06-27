b3973259e0af5d651a7465612c2e2e39ec4079b0
jstym <jstym@noemail.com>
2015-06-27 21:34:30 +0800

    Added pulse rifle.

---
+++ Assets/Scripts/Bullet/BulletSpec_PulseRifle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletSpec_PulseRifle : BulletSpec
+{
+	public BulletSpec_PulseRifle()
+	{
+		timeBetweenBurst = 0.8f;
+		burstRounds = 2;
+		timeBetweenBulletInBurst = 0.2f;
+
+		damagePerShot = 60;
+		bulletLifeTime = 1f;
+		shootForce = 12f;
+		shootSpread = 0.5f;
+	}	
+}
