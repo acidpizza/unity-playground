ca337d20b8dcbeae11c443951cca4d46f2c89ccb
jstym <jstym@noemail.com>
2015-06-30 01:14:46 +0800

    Added AttackBot.
    - Added animations.
    - Have not coded shooting yet.

---
+++ Assets/Scripts/Enemy/EnemyMovement_Shooter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement_Shooter : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    NavMeshAgent nav;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+        if(!enemyHealth.IsDead () && !playerHealth.IsDead ())
+        {
+            nav.SetDestination (player.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+}
