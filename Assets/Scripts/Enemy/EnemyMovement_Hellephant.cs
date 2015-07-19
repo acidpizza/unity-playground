ffc959108ba5f87b68880d8e3e8e2778922ecfcc
jstym <jstym@noemail.com>
2015-07-19 16:52:43 +0800

    Updated layers. Achieved hellephant charging and stomp.

--- Assets/Scripts/Enemy/EnemyMovement_Hellephant.cs
+++ Assets/Scripts/Enemy/EnemyMovement_Hellephant.cs
@@ -1,87 +1,78 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyMovement_Hellephant : MonoBehaviour
 {
     Transform player;
+	Rigidbody playerRigidBody;
     PlayerHealth playerHealth;
     EnemyHealth enemyHealth;
     NavMeshAgent nav;
 	Animator anim;
 
-	bool playerInRange;
+	bool playerInRange = false;
+	bool charging = false;
+	bool chargeFinalBurst = false;
+	float chargeTimer = 999f;
+
 	public float rotateSpeed = 10f;
+	public float chargeTime = 1f;
+	public float chargeReloadTime = 5f;
 
 	// Line Of Sight detection
 	int shootableByEnemyMask;
 	Ray lineOfSightRay;
 	RaycastHit playerRayHit;
 	float range = 100f;
-	//	LineRenderer gunLine;
 
-	public EnemyShooting enemyShooting;
+	int enemyLayer;
+	int hellephantEtheralLayer;
 
+	public SkinnedMeshRenderer hellephantRenderer;
+	public Material hellephantNormalMaterial;
+	public Material hellephantTransparentMaterial;
+
+	enum EtheralState
+	{
+		Normal, Etheral
+	};
+	Color normalColor = new Color(1, 1, 1, 1);
+	Color etheralColor = new Color(1, 1, 1, 0.2f);
+	
     void Awake ()
     {
         player = GameObject.FindGameObjectWithTag ("Player").transform;
+		playerRigidBody = player.gameObject.GetComponent<Rigidbody> ();
         playerHealth = player.GetComponent <PlayerHealth> ();
         enemyHealth = GetComponent <EnemyHealth> ();
         nav = GetComponent <NavMeshAgent> ();
 		anim = GetComponent <Animator> ();
 
-		shootableByEnemyMask = LayerMask.GetMask ("ShootableByEnemy");
-//		gunLine = GetComponent<LineRenderer> ();
+		enemyLayer = LayerMask.NameToLayer("Enemy");
+		shootableByEnemyMask = ~enemyLayer;
+		hellephantEtheralLayer = LayerMask.NameToLayer("HellephantEtheral");
     }
 
 
     void Update ()
     {
+		chargeTimer += Time.deltaTime;
+
         if(!enemyHealth.IsDead () && !playerHealth.IsDead ())
         {
-			if(playerInRange)
+			if(charging)
 			{
-				lineOfSightRay.origin = transform.position + new Vector3(0,0.2f,0);
-				lineOfSightRay.direction = player.position - transform.position;
-
-            	if(HasLineOfSightToPlayer())
-				{
-					nav.enabled = false;
-					if(TurnAndIsFacingPlayer())
-					{
-						anim.SetBool("PlayerInSight", true);
-						enemyShooting.IsShooting = true;
-					}
-					else
-					{
-						anim.SetBool("PlayerInSight", false);
-						enemyShooting.IsShooting = false;
-					}
-				}
-				else
-				{
-					// Player is in range but is blocked. Continue chasing to get line of sight.
-					anim.SetBool("PlayerInSight", false);
-					enemyShooting.IsShooting = false;
-
-					nav.enabled = true;
-					nav.SetDestination (player.position);
-				}
+				ChargeToPlayer();
 			}
 			else
 			{
-				// Chase the player until player is in range
-				anim.SetBool("PlayerInSight", false);
-				enemyShooting.IsShooting = false;
-
-				nav.enabled = true;
-				nav.SetDestination (player.position);
+				ChasePlayer();
 			}
         }
         else
         {
 			// Enemy or Player dead
             nav.enabled = false;
-			enemyShooting.IsShooting = false;
 
 			if(playerHealth.IsDead())
 			{
@@ -90,6 +81,102 @@ public class EnemyMovement_Hellephant : MonoBehaviour
         }
     }
 
+	void ChasePlayer()
+	{
+		if(playerInRange)
+		{
+			lineOfSightRay.origin = transform.position + new Vector3(0,0.2f,0);
+			lineOfSightRay.direction = player.position - transform.position;
+			
+			if(HasLineOfSightToPlayer() && chargeTimer > chargeReloadTime)
+			{
+				// Charge to the player
+				StartCharge();
+			}
+			else
+			{
+				// Player is in range but is blocked. Continue chasing to get line of sight.
+			}
+		}
+		else
+		{
+			// Chase the player until player is in range
+		}
+
+		if(nav.enabled)
+		{
+			nav.SetDestination (player.position);
+		}
+	}
+
+	void StartCharge()
+	{
+		chargeTimer = 0f;
+		charging = true;
+		nav.speed = 15;
+		chargeFinalBurst = false;
+		TurnEtheral ();
+	}
+
+	void EndCharge()
+	{
+		charging = false;
+		nav.speed = 2;
+		chargeFinalBurst = false;
+		TurnNormal ();
+		Stomp ();
+	}
+
+	void ChargeToPlayer()
+	{
+		if(!chargeFinalBurst)
+		{
+			// Initial phase of charge -> keep following player
+			if(nav.remainingDistance > 4.0f)
+			{
+				nav.SetDestination (player.position);
+			}
+			else
+			{
+				// Near to the player -> predict movement and charge there
+				chargeFinalBurst = true;
+				Vector3 anticpatedLocation = player.position + playerRigidBody.velocity * 0.75f;
+				nav.SetDestination (anticpatedLocation);
+			}
+		}
+		else if(nav.remainingDistance < 1f)
+		{
+			EndCharge();
+		}
+	}
+
+	void Stomp()
+	{
+		anim.SetTrigger ("Stomp");
+		nav.enabled = false;
+		Invoke ("EnableNav", 0.8f);
+	}
+
+	void EnableNav()
+	{
+		if(!enemyHealth.IsDead () && !playerHealth.IsDead ())
+		{
+			nav.enabled = true;
+		}
+	}
+
+	void TurnEtheral()
+	{
+		gameObject.layer = hellephantEtheralLayer;
+		hellephantRenderer.material = hellephantTransparentMaterial;
+	}
+
+	void TurnNormal()
+	{
+		gameObject.layer = enemyLayer;
+		hellephantRenderer.material = hellephantNormalMaterial;
+	}
+
 	void OnTriggerEnter (Collider other)
 	{
 		if(other.gameObject == player.gameObject)
@@ -97,38 +184,17 @@ public class EnemyMovement_Hellephant : MonoBehaviour
 			playerInRange = true;
 		}
 	}
-	
-	
+
 	void OnTriggerExit (Collider other)
 	{
 		if(other.gameObject == player.gameObject)
 		{
 			playerInRange = false;
-//			gunLine.enabled = false;
 		}
 	}
 
 	bool HasLineOfSightToPlayer()
 	{
 		return Physics.Raycast (lineOfSightRay, out playerRayHit, range, shootableByEnemyMask) && playerRayHit.transform.tag == "Player";
-		/*
-		if(Physics.Raycast (lineOfSightRay, out playerRayHit, range, shootableByEnemyMask))
-		{
-			gunLine.enabled = true;
-			gunLine.SetPosition (0, transform.position + new Vector3(0,0.2f,0));
-			gunLine.SetPosition (1, playerRayHit.point);
-
-			return playerRayHit.transform.tag == "Player";
-		}
-		gunLine.enabled = false;
-		return false;
-		*/
-	}
-
-	bool TurnAndIsFacingPlayer()
-	{
-		Quaternion wantedRot=Quaternion.LookRotation(lineOfSightRay.direction);
-		transform.rotation=Quaternion.Slerp(transform.rotation, wantedRot, rotateSpeed*Time.deltaTime);
-		return (Quaternion.Angle (transform.rotation, wantedRot) < 5);
 	}
 }
