e265765db9498e2594f913e31f6216e0d16bf173
jstym <jstym@noemail.com>
2015-07-21 00:23:43 +0800

    Added meteor strike and flames animation and damage.

---
+++ Assets/Scripts/EnemyBullets/FireScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireScript : MonoBehaviour {
+
+	public float lifetime = 10f;
+	public float timeBetweenDamage = 1f;
+	public int damage = 10;
+
+	float timer_lifetime = 0f;
+	float timer_damage = 0f;
+	GameObject player;
+	ParticleSystem fireParticleSystem;
+	PlayerHealth playerHealth;
+	bool playerInRange;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealth = player.GetComponent <PlayerHealth> ();
+
+		fireParticleSystem = GetComponent<ParticleSystem> ();
+		Destroy (gameObject, lifetime + 3f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		timer_lifetime += Time.deltaTime;
+		timer_damage += Time.deltaTime;
+
+		if (timer_lifetime > lifetime) 
+		{
+			fireParticleSystem.loop = false;
+		}
+
+		if(timer_damage >= timeBetweenDamage && playerInRange)
+		{
+			Attack ();
+		}
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInRange = true;
+		}
+	}
+	
+	
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInRange = false;
+		}
+	}
+
+	void Attack ()
+	{
+		timer_damage = 0f;
+		
+		if(!playerHealth.IsDead ())
+		{
+			playerHealth.TakeDamage ("Meteor Flames", damage);
+		}
+	}
+}
---
+++ Assets/Scripts/EnemyBullets/MeteorStrike.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeteorStrike : MonoBehaviour {
+
+	public Transform firePrefab;
+	public float strikeRadius = 1.2f;
+	public int damage = 15;
+
+	ParticleSystem meteorParticleSystem;
+	ParticleCollisionEvent[] collisionEvents;
+
+	Transform player;
+	PlayerHealth playerHealth;
+
+	void Awake()
+	{
+		meteorParticleSystem = GetComponent<ParticleSystem> ();
+		collisionEvents = new ParticleCollisionEvent[16];
+
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		playerHealth = player.GetComponent <PlayerHealth> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	void OnParticleCollision(GameObject other)
+	{
+		int numCollisionEvents = meteorParticleSystem.GetCollisionEvents (other, collisionEvents);
+
+		if(numCollisionEvents > 0)
+		{
+			Instantiate(firePrefab.gameObject, collisionEvents[0].intersection, firePrefab.rotation);
+
+			float distanceToPlayer = (collisionEvents[0].intersection - player.position).magnitude;
+			if(distanceToPlayer < strikeRadius)
+			{
+				playerHealth.TakeDamage("Meteor Strike", damage);
+			}
+		}
+	}
+
+}
